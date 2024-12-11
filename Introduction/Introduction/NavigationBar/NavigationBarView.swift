a7ffc1ff231787452c92079ecd6e8a116aecb239
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-12-11 16:16:07 +0100

    Making your navigation title editable

--- Introduction/Introduction/NavigationBar/NavigationBarView.swift
+++ Introduction/Introduction/NavigationBar/NavigationBarView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct NavigationBarView: View {
+    @State private var navigationTitle = "SwiftUI"
+    
     var body: some View {
         NavigationStack {
             List(0..<100) { i in
                 Text("Row \(i)")
             }
-            .navigationTitle("SwiftUI")
+            .navigationTitle($navigationTitle)
             .navigationBarTitleDisplayMode(.inline)
             .toolbarBackground(.blue)
             .toolbarColorScheme(.dark)
