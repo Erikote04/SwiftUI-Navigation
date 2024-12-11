2f87a7ad827f56bac8a2c5acce34691c8516d809
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-12-11 16:14:07 +0100

    Customizing the navigation bar appearance

---
+++ Introduction/Introduction/NavigationBar/NavigationBarView.swift
@@ -0,0 +1,26 @@
+//
+//  NavigationBarView.swift
+//  Introduction
+//
+//  Created by Erik Sebastian de Erice Jerez on 11/12/24.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        NavigationStack {
+            List(0..<100) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue)
+            .toolbarColorScheme(.dark)
+        }
+    }
+}
+
+#Preview {
+    NavigationBarView()
+}
