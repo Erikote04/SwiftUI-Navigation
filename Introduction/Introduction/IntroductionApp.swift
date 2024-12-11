f0a76f3192f7c940f74d28f68eb3692f0c77ed9a
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-12-11 15:40:56 +0100

    Setting up introduction project

---
+++ Introduction/Introduction/IntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroductionApp.swift
+//  Introduction
+//
+//  Created by Erik Sebastian de Erice Jerez on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct IntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
