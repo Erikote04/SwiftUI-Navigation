6a7860484e03c485cc7aa42c9a960d5a13dfca19
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-03-02 21:35:02 +0100

    Deleting "not sure" documentation

--- Navigation/Moonshot/Coordinator/BaseCoordinator.swift
+++ Navigation/Moonshot/Coordinator/BaseCoordinator.swift
@@ -63,25 +63,6 @@ class BaseCoordinator: BaseCoordinatorProtocol, ObservableObject {
     }
 }
 
-/// Presenting a sheet inside another sheet or a fullScreenCover can sometimes have unexpected behaviors.
-/// This flow should be avoided, but if you still want to do it, here are some advertisements:
-
-/// You CAN'T define the sheets in the same view.
-/// You MUST define the action in the corresponding view
-
-/// Here's an example of what not to do. This example will not cause problems if the sheet(s) and/or fullScreenCover(s) are not related:
-
-/// coordinator.build(.home)
-///     .navigationDestination(for: AppView.self) { view in
-///         coordinator.build(view)
-///     }
-///     .sheet(item: $coordinator.sheet) { sheet in
-///         coordinator.build(sheet)
-///     }
-///     .fullScreenCover(item: $coordinator.fullScreenCover) { fullScreenCover in
-///         coordinator.build(fullScreenCover)
-///     }
-
 /// If you want to navigate inside a sheet or a fullScreenCover you must implement the following lines inside the corresponding build function
 
 /// NavigationStack(path: Binding(get: { self.path }, set: { self.path = $0 })) {
