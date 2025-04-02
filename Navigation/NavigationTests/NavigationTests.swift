58a86b8eb8611600eb1a9dc752b23e357427ca69
Erik Sebastián de Erice Jerez <eriks.deerice@vasscompany.com>
2025-04-02 23:43:50 +0200

    Added new target for testing

---
+++ Navigation/NavigationTests/NavigationTests.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationTests.swift
+//  NavigationTests
+//
+//  Created by Erik Sebastian de Erice Jerez on 2/4/25.
+//
+
+import Testing
+
+struct NavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
